63848210d2d7e065e786dd5a350a8480e3c63181
Jeff Lee <leej306@Jeffs-MacBook-Pro.local>
2020-10-11 23:08:24 -0500

    sketch triangle with bells

--- Javascript/sketch.js
+++ Javascript/sketch.js
@@ -8,37 +8,40 @@ function setup() {
   // create canvas 500 by 500 px:
   createCanvas(windowWidth,windowHeight);
 
-  background('rgba(0,255,0, 0.25)');
-
-
+  background(110, 0, 0);
+//print(windowWidth)
+//change rectangle x,y cordinates to the center of the rect
+rectMode(CENTER);
 }
 
 function draw() {
   smooth();
-  //styles for the points
-  fill(120);
-  stroke(40,200,100);
-  strokeWeight(10);
-//point is only x,y cordinate
-point(mouseX,mouseY);
 //style for ellipse
-noStroke();
+
   // put drawing code here to run in a loop
-ellipse(300, 500, 150, 150);
+ellipse(mouseX, mouseY, 100, 200);
 
 
 //styles for the rect
 stroke(100,255,80);
 strokeWeight(20);
 fill(50,200,120);
-rect(800,300,200,300);
+rect(0,0,0,0);
 
 //style for the triangle
 stroke(40,100,100);
 strokeWeight(20);
-noFill();
-triangle(1200,50,1300,600,800,950,400);
+fill(179, 212, 245);
+triangle(1200,50,1300,600,mouseX,mouseY);
 
 
+
+//styles for the points
+fill(255, 233, 69);
+stroke(109, 83, 224);
+strokeWeight(10);
+//point is only x and y cordinate
+print(mouseX, mouseY);
 print(mouseX);
+
 }
