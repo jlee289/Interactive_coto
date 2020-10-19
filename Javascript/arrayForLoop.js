d73c507cc9a58c95a6ce21f843d9011d5a346da6
Jeff Lee <leej306@Jeffs-MacBook-Pro.local>
2020-10-19 00:57:40 -0500

    itachi loop

--- Javascript/arrayForLoop.js
+++ Javascript/arrayForLoop.js
@@ -2,6 +2,10 @@
 let itachi;
 let posX;
 let posY;
+let randName;
+
+let squareNums = [25, 36, 49, 64, 82, 100, 121, 144];
+let names = ['jon', 'susie', 'maranda'];
 
 function preload(){
 itachi = createImg("images/Itachi.png");
@@ -10,24 +14,39 @@ itachi = createImg("images/Itachi.png");
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
-
+  rectMode(CENTER);
   imageMode(CENTER);
-  for(let i=0; i < 1000; i++){
-    posX = random(windowWidth);
-    posY = random(windowHeight);
-    fill(random (225), random (225),random (225));
-    ellipse(posX, posY, 70, 70);
-  images(itachi, posX, posY, 10, 10);
+
+frameRate(10);
 
 }
-}
+randName = int(random(names.length));
+
+
+  for(let i=0; i < windowWidth; i++);
+  line(i*40, 0,i*40, windowHeight);
+
 
 function draw(){
-stroke(140,100,200);
-  for(let i=0; i< windowHeight; i++){
+  background(20,40,10);
+
+  text(names[randName], 200,300);
+
+  for(let i=0; i < 500; i++){
+      posX = random(windowWidth);
+      posY = random(windowHeight);
+    fill(random (225), random (225),random (225));
+    ellipse(posX, posY, 120, 120);
+    image(itachi, posX, posY, 80, 80);
+  }
+    stroke(140,100,200);
+    for(let i=0; i< windowHeight; i++){
     line(0, i*40, windowWidth, i*40);
   }
 
-  for(let i=0; i<windowWidth; i++);
-  line(i*40, 0,i*40, windowHeight)
+  for(i=0; i<squareNums.length; i++){
+    print(squareNums[i]);
+    rect(random(windowWidth), random(windowHeight), squareNums[i], squareNums[i]);
+    fill(random (225), random (225),random (225));
+  }
 }
